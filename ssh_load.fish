e15faceb1ac3eb524a02f21a40419863421f1010
edd255 <101655959+edd255@users.noreply.github.com>
2026-04-15 22:53:36 +0200

    chore: added file extensions

--- ssh_load.fish
+++ ssh_load.fish
@@ -5,7 +5,7 @@ set SSH_KEYS_TO_AUTOLOAD \
 
 eval (keychain --dir "$XDG_RUNTIME_DIR" --absolute --eval --quiet)
 set -x DISPLAY :0
-set -x SSH_ASKPASS "$HOME/.local/bin/ssh_pass"
+set -x SSH_ASKPASS "$HOME/.local/bin/ssh_pass.fish"
 for key in $SSH_KEYS_TO_AUTOLOAD
     set -x SSH_KEY_BEING_ADDED $key
     setsid ssh-add $key < /dev/null
