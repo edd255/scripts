7ced67999908caf136b6ff773dd840013bbbba3d
edd255 <101655959+edd255@users.noreply.github.com>
2026-04-30 11:34:56 +0200

    feat(bibtex): added bibtex config

---
+++ bibtex.bash
@@ -0,0 +1,15 @@
+bibtex-tidy                                    \
+    --curly                                    \
+    --numeric                                  \
+    --months                                   \
+    --space=4                                  \
+    --align=13                                 \
+    --blank-lines                              \
+    --sort=type,year,author,title              \
+    --duplicates=key                           \
+    --no-escape                                \
+    --sort-fields                              \
+    --trailing-commas                          \
+    --no-remove-dupe-fields                    \
+    --enclosing-braces=title,journal,booktitle \
+    references.bib
